a8a6ff026750f9c65ae508c6f09473740b83eed5
dmitriy khubulashvili <123072814+blackmortem@users.noreply.github.com>
2023-12-20 04:14:01 -0800

    edit ORM: select all users and add users

--- DataBase/orm.py
+++ DataBase/orm.py
@@ -1,4 +1,5 @@
 # function on the orm
+import email
 from engine import session_factory, engine, Base
 from models import *
 from sqlalchemy.sql import *
@@ -14,24 +15,27 @@ def select_Users_ORM():
 			query = select(Users)
 			result = session.execute(query)
 			all_users = result.all()
+			print(f"{all_users}")
    
 def new_userORM(email: str, password: str, name: str = 'anonim', **id: int):
     """Add new user in tables (Users, Statistics)"""
     with session_factory() as session:
-        user_create = insert(Users).values(email=email, password=password, **id)
-        res = session.execute(user_create)
+        user_create = Users(email=email, password=password, **id)
+        
         # create user in Users table
-        user_create2 = insert(Statistics).values(id=res.inserted_primary_key[0], name=name)
-        session.execute(user_create2)
+        user_create2 = Statistics(**id ,name=name)
+        session.add_all([user_create2,user_create])
         # create user in Statistics table
         session.commit()
-   
-def select_via_email():
+""" 
+def select_via_email(search_email):
   with session_factory() as session:
-    users_email = Users.email
-    select_user_email = session.get(Users, users_email)
-   
-def update_User(new_email, new_password, new_name, id_user,id_stat):
+    
+    select_user_email = session.query(Users.email).filter_by()
+    print(select_user_email)
+
+    
+def update_User(**kwargs):
   id_user = Users.id
   id_stat = Statistics.id
   with session_factory() as session:
@@ -42,3 +46,8 @@ def update_User(new_email, new_password, new_name, id_user,id_stat):
     updates2.name = new_name
     
 create_tables()
+
+select_Users_ORM()
+# new_userORM('asasasa','sasa','sasa' , id = 2)
+# select_via_email('dimahubulwqsq')
+# """
